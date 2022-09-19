84bb18a9605667ac886af9389228970c7164c09c
Paul Robson <paul@robsons.org.uk>
2022-09-19 20:07:38 +0100

    Update

--- source/scripts/tokens.py
+++ source/scripts/tokens.py
@@ -78,6 +78,17 @@ class Token(object):
 	def setLabel(self,label):
 		self.label = label
 
+class PunctuationToken(Token):
+	def __init__(self,name):
+		Token.__init__(self,name,-1)
+		self.precedence = 0
+	def getPrecedence(self):
+		return self.precedence
+	def sortKey(self):
+		return "P"+self.name 
+	def setPrecedence(self,p):
+		self.precedence = p 
+
 class CtrlToken(Token):
 	def sortKey(self):
 		return "0"+str(self.set)+self.name
@@ -111,6 +122,7 @@ class TokenCollection(object):
 			self.addToken("!2:SH2"+str(i),CtrlToken("!2:SH2",i))
 		self.loadTokens()
 		self.allocateIDs() 																# allocate IDs.
+		self.createPunctuationTokens()
 	#
 	#		Load tokens in
 	#
@@ -130,6 +142,37 @@ class TokenCollection(object):
 					newToken = Token(w,int(cClass))
 				self.addToken(w,newToken)
 		self.tokenList.sort(key = lambda x:x.sortKey())									# sort into the correct order
+	#
+	#		Create all punctuation tokens.
+	#
+	def createPunctuationTokens(self):
+		tokens = [ None ] * 64 															# tokens go here.
+		for i in range(32,64): 															# standard
+			tokens[i] = chr(i)
+		for i in range(0,2): 															# double
+			for j in range(0,3):
+				tokens[i*4+j] = "<>"[i]+"<=>"[j]
+		for i in range(64,128):															
+			c = chr(i).upper()
+			if c < 'A' or c > 'Z':
+				tokens[((i & 0x20) >> 2) | (i & 7) | 0x10] = c
+		for i in range(0,64):															# create all the punctuation tokens.
+			newToken = PunctuationToken(tokens[i] if tokens[i] is not None else "!!"+str(i))
+			newToken.setID(i)
+			newToken.setPrecedence(self.getOperatorPrecedence(newToken.getName()))
+			self.addToken(newToken.getName(),newToken)
+	#
+	#		Get operator precedence
+	#
+	def getOperatorPrecedence(self,op):
+		prec = {
+			"&":1,"|":1,"^":1,														
+			">":2,">=":2,"<":2,"<=":2,"=":2,"<>":2,
+			"+":3,"-":3,
+			"*":4,"/":4,"%":4,"<<":4,">>":4,
+			"!":5,"\\":5,"?":5,"$":5
+		} 
+		return prec[op] if op in prec else 0
 	#
 	#		Add a token
 	#
@@ -160,16 +203,49 @@ class TokenCollection(object):
 					constHandle.write("KWD_{0:32} = ${1:02x}; ${1:02x} {2}\n".format(self.processName(t.getName()),t.getID(),t.getName()))
 		textHandle.write("\t.text\t$FF\n")
 	#
+	#		Dump precedence table
+	#
+	def dumpPrecedenceTable(self,h):
+		h.write("PrecedenceLevel:\n".format(set))
+		for t in self.tokenList:
+			if isinstance(t,PunctuationToken):
+				h.write("\t.byte\t{0:2}\t; ${1:02x} {2}\n".format(t.getPrecedence(),t.getID(),t.getName()))
+	#
+	#		Dump vector table
+	#				
+	def dumpVectorTable(self,set,h):
+		h.write("VectorSet{0}:\n".format(set))
+		for t in self.tokenList:
+			if t.getSet() == set:
+				label = t.getLabel() if t.getLabel() is not None else "SyntaxError"
+				h.write("\t.word\t{0:32} ; ${1:02x} {2}\n".format(label,t.getID(),t.getName()))
+	#
 	#		Process Name to remove control characters if any.
 	#
 	def processName(self,s):
 		return s.replace("!","PLING").replace("$","DOLLAR").replace(":","COLON").replace("(","LPAREN")
 
 if __name__ == "__main__":
+	note = ";\n;\tThis is automatically generated.\n;\n"
 	t = TokenCollection()
 	h1 = open("generated/kwdtext.dat","w")
 	h2 = open("generated/kwdconst.inc","w")
+	h1.write(note)
+	h2.write(note)
 	for i in range(0,3):
 		t.dumpGroup(i,h1,h2)
 	h1.close()
 	h2.close()
+
+	h = open("generated/precedence.dat","w")
+	h.write(note)
+	t.dumpPrecedenceTable(h)
+	h.close()
+
+	h = open("generated/vectors.dat","w")
+	h = sys.stdout
+	h.write(note)
+	t.dumpVectorTable(-1,h)
+	for i in range(0,3):
+		t.dumpVectorTable(i,h)
+	h.close()
