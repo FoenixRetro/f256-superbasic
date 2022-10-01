ee92727230bc076092e508d2a1295e9366e17d6a
Paul Robson <paul@robsons.org.uk>
2022-10-01 09:04:25 +0100

    Update

--- source/common/expressions/term/assignstring.asm
+++ source/common/expressions/term/assignstring.asm
@@ -19,30 +19,37 @@
 ; ************************************************************************************************
 
 AssignString:
-		.debug
 		phy
 		;
-		;		Copy new string address to zTemp1
+		;		Copy new string address to zTemp1, and the address to write the reference to to zTemp0
 		;
 		lda 	NSMantissa0+1,x
 		sta 	zTemp1
 		lda 	NSMantissa1+1,x
 		sta 	zTemp1+1
 		;
+		lda 	NSMantissa0,x
+		sta 	zTemp0
+		lda 	NSMantissa1,x
+		sta 	zTemp0+1		
+		;
+		;
 		;		See if there's a concrete string in the target already.
 		;
-		lda 	NSMantissa1,x 				; the high byte of the target address
+		ldy 	#1 							; the high byte of the target address
+		lda 	(zTemp0),y
 		beq 	_ASNewStringRequired 		; if zero, a new string is required.
 		;
 		;		If so does it fit in the space available.
 		;
-		sec 								; put Address-2 in zTemp0 - this is the size/status/string record
-		lda 	NSMantissa0,x 				; of concreted strings
+		sec 								; put Address-2 in zsTemp - this is the size/status/string record		
+		lda 	(zTemp0) 					; of concreted strings
 		sbc 	#2
-		sta 	zTemp0
-		lda 	NSMantissa1,x
+		sta 	zsTemp
+		ldy 	#1
+		lda 	(zTemp0),y
 		sbc 	#0
-		sta 	zTemp0+1
+		sta 	zsTemp+1
 		;
 		ldy 	#$FF 						; get length of new string
 _ASGetLength:
@@ -50,19 +57,19 @@ _ASGetLength:
 		lda 	(zTemp1),y
 		bne 	_ASGetLength
 		tya 								; is this length <= current length
-		cmp 	(zTemp0)
+		cmp 	(zsTemp)
 		bcc 	_ASCopyString
 		beq 	_ASCopyString
 		;
 		lda 	#$80 						; mark as unused.
 		ldy 	#1
-		sta 	(zTemp0),y
+		sta 	(zsTemp),y
 		;
 		; 		Concrete new if required
 		;
 _ASNewStringRequired:	
 		inx 								; concrete the new string.
-		jsr 	StringConcrete				; (breaks zTemp1/2, not zTemp0)
+		jsr 	StringConcrete				; (breaks zTemp1/2, not zTemp0 and zsTemp)
 		dex	
 		clc
 		lda 	NSMantissa0+1,x 			; copy that new address to the reference.
@@ -74,7 +81,7 @@ _ASNewStringRequired:
 		sta 	(zTemp0),y
 		bra 	_ASExit
 		;
-		; 		Copy the string at zTemp1 to zTemp0+2 (skipping the header bit.)
+		; 		Copy the string at zTemp1 to zsTemp (skipping the header bit.)
 		;
 _ASCopyString:
 		ldy 	#0
@@ -82,8 +89,7 @@ _ASCopyLoop:
 		lda 	(zTemp1),y
 		iny
 		iny
-		sta 	(zTemp0),y
-		dey
+		sta 	(zsTemp),y
 		dey
 		cmp 	#0
 		bne 	_ASCopyLoop		
