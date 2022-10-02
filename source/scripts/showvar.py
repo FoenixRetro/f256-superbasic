e24eeed6483adbb20bfa19c686b9b77e5388d859
Paul Robson <paul@robsons.org.uk>
2022-10-02 16:47:21 +0100

    Update

--- source/scripts/showvar.py
+++ source/scripts/showvar.py
@@ -12,6 +12,20 @@
 import os,sys,re
 from showstack import *
 
+def getData(md,a,t):
+	return md.decode(md.readLong(a),md.read(a+4),t)
+	return "{0:x}".format(a)
+
+def extractArray(md,v,x2):
+	a1 = md.read(v+5)
+	p = md.readWord(v+3)
+	size = (2 if (md.read(v+2) & 0x10) != 0 else 5)
+	p = p + x2 * (a1+1) * size
+	array = "\t\t\t\t\t[" 
+	array = array+",".join([getData(md,p + i * size,md.read(v+2)) for i in range(0,a1+1)])
+	return array+"]"
+	
+
 if __name__ == "__main__":
 	ls = LabelStore()
 	md = MemoryDump()
@@ -21,6 +35,7 @@ if __name__ == "__main__":
 	vs = ls.get("VariableSpace")
 	while md.read(vs) != 0:
 		name = ""
+		isArray = False
 		p = vs + 8
 		done = False
 		while not done:
@@ -35,12 +50,22 @@ if __name__ == "__main__":
 			if a2 != 0:
 				name += ",{0}".format(a2)
 			name += ")"
+			isArray = True
 		else:
 			v = md.decode(md.readLong(vs+3),md.read(vs+7),t)
 		if (t & 0x18) == 0x18:
 			v = "(procedure)"
 			name += ")"
 		print("\t@${0:04x} {1:16} [${2:02x}] = {3}".format(vs,name,md.read(vs+2),v))
+		if isArray:
+			a2 = md.read(vs+6)
+			if a2 == 0:
+				print("{0}".format(extractArray(md,vs,0)))
+			else:
+				print("\t\t\t\t\t[")
+				for i in range(0,a2+1):
+					print("\t{0}".format(extractArray(md,vs,i)))
+				print("\t\t\t\t\t]")
 		vs += md.read(vs)
 
 
