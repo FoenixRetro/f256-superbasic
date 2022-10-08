c78ef448dc94ecbfc3161cb5a6f074c5bad8e070
Paul Robson <paul@robsons.org.uk>
2022-10-08 18:55:57 +0100

    Update

---
+++ documents/circle.py
@@ -0,0 +1,46 @@
+# *******************************************************************************************
+# *******************************************************************************************
+#
+#		Name : 		circle.py
+#		Purpose :	Bresenham circle algorithm (we will stretch it)
+#		Date :		8th October 2022
+#		Author : 	Paul Robson (paul@robsons.org.uk)
+#
+# *******************************************************************************************
+# *******************************************************************************************
+
+from PIL import Image,ImageDraw
+import math
+
+def plot(draw,x,y):
+	draw.point((x+160,y+120),fill = (0,255,0))
+
+screen = Image.new("RGB",(320,240))
+draw = ImageDraw.Draw(screen)
+
+r = 100
+x = 0
+y = r 
+d = 3 - 2 * r 
+
+def plot2(draw,x,y):
+	plot(draw,x*3/2,y)			# scale here to make ellipse
+	plot(draw,x*3/2,-y)
+
+def plot1(draw,x,y):
+	plot2(draw,x,y)
+	plot2(draw,y,x)
+
+while x <= y:
+	plot1(draw,x,y)
+	if d < 0:
+		x += 1
+		d = d + 4 * x + 6
+	else:
+		x += 1
+		y -= 1
+		d = d + 2 * 2 * ((x-y) + 5)
+
+
+
+screen.show()
