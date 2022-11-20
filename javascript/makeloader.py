bd4ea6d0e5b2298492ae0d8f6aaf4ce076740fef
Paul Robson <paul@robsons.org.uk>
2022-11-20 12:17:50 +0000

    Update

--- javascript/makeloader.py
+++ javascript/makeloader.py
@@ -9,9 +9,14 @@
 # *******************************************************************************************
 # *******************************************************************************************
 
-import os,sys,math,datetime
+import os,sys,math,datetime,re
 
-h = open("loaders.h","w")
+defined = {}
+for s in [x.lower() for x in sys.argv[1:]]:
+	if s != "-d":
+		m = re.match("^(.*?)_address=0x(.*)$",s)
+		defined[m.group(1).lower()] = int(m.group(2),16)
+		h = open("loaders.h","w")
 
 for root,dirs,files in os.walk("loading"):
 	for f in files:
@@ -19,5 +24,9 @@ for root,dirs,files in os.walk("loading"):
 		parts = f.split(".")
 		binary = [x for x in open(file,"rb").read(-1)]
 		h.write("static const BYTE8 {0}_image[] = {{ {1} }};\n".format(parts[0],",".join([str(x) for x in binary])))
-		h.write("CPUCopyROM({1},{2},{0}_image);\n".format(parts[0],int(parts[1],16),len(binary)))
+		if parts[1].lower() in defined:
+			a = defined[parts[1].lower()]
+		else:
+			a = int(parts[1],16)
+		h.write("CPUCopyROM(0x{1:x},{2},{0}_image);\n".format(parts[0],a,len(binary)))
 h.close()
