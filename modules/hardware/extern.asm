57bcd9be00d64e87f23c1457b46ccaab39274795
Paul Robson <paul@robsons.org.uk>
2022-12-22 10:21:18 +0000

    Update

--- modules/hardware/extern.asm
+++ modules/hardware/extern.asm
@@ -46,54 +46,7 @@ _EXMoveDown: 								; move down past prompt
 		stz 	1
 		rts				
 
-; ************************************************************************************************
-;
-;											Get Character
-;
-;	Returns:
-;			8 		Backspace, if not far left
-;			9 		Tab spacing (Ctrl+I)
-;			13 		CR/LF with scrolling if required
-;			32..127	Corresponding ASCII character
-;
-; ************************************************************************************************
 
-Export_EXTInputSingleCharacter:
-PagedInputSingleCharacter:
-		phx
-		phy
-_EISCWait:	
-		.tickcheck PagedSNDUpdate 			; sound processing carries on.
-		jsr 	$FFE4 						; get a key
-		cmp 	#0 							; loop back if none pressed.
-		beq 	_EISCWait
-		ply
-		plx
-		rts
-
-; ************************************************************************************************
-;
-;				Break Check. Checks Ctrl+C, Escape or whatever. Returns Z if pressed
-;
-; ************************************************************************************************
-
-Export_EXTBreakCheck:
-		jmp		$FFE1
-
-; ************************************************************************************************
-;
-;						Read Game Controller A -> A (Button1/Right/Left/Down/Up)
-;
-; ************************************************************************************************
-
-Export_EXTReadController:
-		phx
-		ldx 	1 							; save current I/O in X
-		stz 	1 							; switch to I/O 0
-		lda 	$DC00  						; read VIA register
-		stx 	1 							; repair old I/O and exit
-		plx
-		rts
 
 		.send code
 		
