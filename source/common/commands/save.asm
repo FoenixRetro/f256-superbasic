7dc7994ad90d5b33a74dc535ce8208bc81988787
Paul Robson <paul@robsons.org.uk>
2022-12-31 11:08:59 +0000

    Update

--- source/common/commands/save.asm
+++ source/common/commands/save.asm
@@ -27,13 +27,20 @@ Command_Save: ;; [SAVE]
 		bcs 	_CSErrorHandler 			; error, so fail.
 		sta 	CurrentFileStream 			; save the reading stream.
 
-		ldx 	#$80
-		stx 	zTemp0+1
-		stz 	zTemp0
-		ldx 	#32
-		lda 	CurrentFileStream
-		jsr 	KNLWriteBlock
+		.cresetcodepointer 					; prepare to loop through code.
+_CSLoop:
+		.cget0 								; any more ?
+		beq 	_CSExit
+		jsr 	CSGetCleanLine
+		sty 	zTemp0+1 					; save write address of data
+		sta 	zTemp0
+		lda 	CurrentFileStream 			; stream to write, count already in X
+		jsr 	KNLWriteBlock 				; write it out.
+		; 
+		.cnextline 							; go to next line.
+		bra 	_CSLoop
 
+_CSExit:
 		lda 	CurrentFileStream 			; close file
 		jsr 	KNLCloseFile
 
@@ -41,6 +48,37 @@ Command_Save: ;; [SAVE]
 
 _CSErrorHandler:
 		jmp 	CLErrorHandler
+
+; ************************************************************************************************
+;
+;					Strip control codes from tokenised line, append CR, len in X
+;
+; ************************************************************************************************
+
+CSGetCleanLine:
+		lda 	#0 							; no indent.
+		jsr 	TKListConvertLine 			; convert line into token Buffer
+
+		ldx 	#0 							; copy stripping controls.
+		ldy 	#0
+_CSClean:
+		lda 	tokenBuffer,y
+		beq 	_CSDoneClean
+		bmi 	_CSIgnoreCharacter
+		sta 	lineBuffer,x
+		inx
+_CSIgnoreCharacter:
+		iny
+		bra 	_CSClean		
+_CSDoneClean:
+		lda 	#13 						; add CR, length now in X and ASCIIZ.
+		sta 	lineBuffer,x
+		inx
+		stz 	lineBuffer,x
+
+		ldy 	#(lineBuffer >> 8) 			; line address in YA
+		lda 	#(lineBuffer & $FF) 	
+		rts
 
 		.send 	code
 
