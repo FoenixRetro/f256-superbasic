3d0ff302559d4bd5f678d90c081a9b81c8dcd152
Paul Robson <paul@robsons.org.uk>
2022-12-31 15:13:48 +0000

    Update

--- test/genbas/makepgm.py
+++ test/genbas/makepgm.py
@@ -1,5 +1,7 @@
+import random
 for sz in [5,10,25,50,100,200,300,500,750,1000]:
 	h = open("r{0}".format(sz),"w")
 	for n in range(0,sz):
-		h.write("{0} rem \"abcdefghijklmnopqrstuvwxyz\"\n".format(n*10+10))
+		s = "".join([chr(random.randint(65,120)) for x in range(0,random.randint(15,25))])
+		h.write("{0} rem \"{1}\"\n".format(n*10+10,s))
 	h.close()
