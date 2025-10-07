b1c38a18314984b03560c7dc1d6a6bf0bbd4b899
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-10-06 19:53:39 -0500

    misc: support variable-height boostscreen rendering
    fixes Jr Jr bootscreen

--- modules/hardware/header/header.asm
+++ modules/hardware/header/header.asm
@@ -1,83 +1,87 @@
-; ************************************************************************************************
-; ************************************************************************************************
-;
-;		Name:		header.asm
-;		Purpose:	Display the header/boot display
-;		Created:	14th December 2022
-;		Reviewed: 	No
-;		Author:		Paul Robson (paul@robsons.org.uk)
-;
-; ************************************************************************************************
-; ************************************************************************************************
+;;
+; Display the boot header for the machine.
+;;
 
 		.section code
 
-; ************************************************************************************************
-;
-;											Show header
-;
-; ************************************************************************************************
+is_jr	.macro
+		stz 	$0001
+		lda 	$D6A7
+		and 	#$10
+		.endm
 
+;;
+; Display the boot header graphics.
+;
+; Selects the appropriate header assets for JR/KR hardware variants, unpacks the
+; RLE-compressed character/attribute data into screen memory, and loads the boot
+; palette.
+;
+; \out Y        Starting line number for the info text
+; \sideeffects  - Modifies zero-page locations $0001, `zTemp0`, and `zTemp1`.
+;               - Modifies `A`, `X`, and `Y` registers
+;               - Writes to $C000 screen memory and $D800/$D840 palette registers.
+;;
 EXTShowHeader:
-		lda 	1
+		lda 	$0008+3
 		pha
-		lda 	8+3
-		pha
-		lda 	8+4
+		lda 	$0008+4
 		clc
 		adc 	#3
-		sta 	8+3
+		sta 	$0008+3
 		;
 		ldx 	#(Header_jchars & $FF)
 		ldy 	#(Header_jchars >> 8)-$40
 
-		stz 	1
-		lda 	$D6A7
-		and 	#$10
+		.is_jr
 		beq 	_EXTSHNotK1
 
 		ldx 	#(Header_kchars & $FF)
 		ldy 	#(Header_kchars >> 8)-$40
-_EXTSHNotK1:		
+_EXTSHNotK1:
 		lda 	#2
 		jsr 	_ESHCopyBlock
 		;
 		ldx 	#(Header_jattrs & $FF)
 		ldy 	#(Header_jattrs >> 8)-$40
 
-		stz 	1
-		lda 	$D6A7
-		and 	#$10
+		.is_jr
 		beq 	_EXTSHNotK2
 
 		ldx 	#(Header_kattrs & $FF)
 		ldy 	#(Header_kattrs >> 8)-$40
-_EXTSHNotK2:		
+_EXTSHNotK2:
 		lda 	#3
 		jsr 	_ESHCopyBlock
 		;
-		stz 	1
+		stz 	$0001
 		ldx 	#16*4-1
 _EXTCopyLUT:
 		lda 	Header_Palette-$4000,x
 		sta 	$D800,x
 		sta 	$D840,x
 		dex
-		bpl 	_EXTCopyLUT		
+		bpl 	_EXTCopyLUT
 		pla
-		sta 	8+3
-		pla
+		sta 	$0008+3
+
+		ldy 	#Header_jinfo_line
+		.is_jr
+		beq 	_exit
+		ldy 	#Header_kinfo_line
+
+	_exit:
 		rts
 
 _ESHCopyBlock:
-		sta 	1 
+		sta 	$0001
 		stx 	zTemp0 						; zTemp0 is RLE packed data
 		sty 	zTemp0+1
 		.set16 	zTemp1,$C000 				; where it goes.
 _ESHCopyLoop:
 		lda 	(zTemp0) 					; get next character
 		cmp 	#Header_RLE 				; packed ?
-		beq 	_ESHUnpack 				
+		beq 	_ESHUnpack
 		sta 	(zTemp1) 					; copy it out.
 		lda 	#1 							; source add 1
 		ldy 	#1 							; dest add 1
@@ -89,7 +93,7 @@ _ESHNext:
 		inc 	zTemp0+1
 _ESHNoCarry:
 		tya 								; zTemp1 + Y
-		clc				
+		clc
 		adc 	zTemp1
 		sta 	zTemp1
 		bcc 	_ESHCopyLoop
@@ -102,13 +106,13 @@ _ESHUnpack:
 		tax
 		dey 								; byte into A
 		lda 	(zTemp0),y
-		beq 	_ESHExit 					; exit if zero.		
+		beq 	_ESHExit 					; exit if zero.
 		ldy 	#0 							; copy start position
 _ESHCopyOut:
 		sta 	(zTemp1),y
 		iny
 		dex
-		bne 	_ESHCopyOut				
+		bne 	_ESHCopyOut
 		lda 	#3 							; Y is bytes on screen, 3 bytes from source
 		bra 	_ESHNext
 		;
@@ -116,14 +120,3 @@ _ESHExit:
 		rts
 
 		.send code
-
-; ************************************************************************************************
-;
-;									Changes and Updates
-;
-; ************************************************************************************************
-;
-;		Date			Notes
-;		==== 			=====
-;
-; ************************************************************************************************
